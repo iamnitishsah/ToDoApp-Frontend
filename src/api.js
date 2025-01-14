c409b2f2ce969781e130091c780697e244a6bf04
Nitish Kumar <iamnitishsah12@gmail.com>
2025-01-15 00:22:55 +0530

    Updated api.js

--- src/api.js
+++ src/api.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 import { ACCESS_TOKEN } from "./constants.js";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-})
+    baseURL: import.meta.env.VITE_API_URL || 'https://todoapp-backend-sb7c.onrender.com',
+});
+
 
 api.interceptors.request.use(
     (config) => {
